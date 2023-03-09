85c6702fe4130d2871506200b3de2d7f0e3f99ef
tanveerisonline <tanveerisonline@gmail.com>
2023-03-09 13:30:45 +0530

    Create person, student and teacher classes

---
+++ person.rb
@@ -0,0 +1,40 @@
+require_relative 'nameable'
+require_relative 'decorator'
+require_relative 'capitalizedecorator'
+require_relative 'trimmerdecorator'
+
+class Person < Nameable
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  # rubocop:disable Naming/PredicateName
+  def is_of_age?
+    age >= 18
+  end
+  # rubocop:enable Naming/PredicateName
+
+  def can_use_services?
+    is_of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
+  end
+
+  private :is_of_age?
+end
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
